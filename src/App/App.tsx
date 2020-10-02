38ca69eb7c068a7693169a6d5c0adbc978f7102b
eyderacm <eyderacm@gmail.com>
2020-10-02 12:15:35 -0500

    setup: add react-router-dom dependencies

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react"
 
 import GlobalStyles from "./GlobalStyles"
+import { Routes } from "./Routes"
 
 export const App = () => (
   <Fragment>
     <GlobalStyles />
+    <Routes />
   </Fragment>
 )
