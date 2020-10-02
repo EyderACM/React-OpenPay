c5a04474c0bd4615c7bd420a040bca11e55db95b
eyderacm <eyderacm@gmail.com>
2020-10-02 14:03:57 -0500

    feat: add justify property to wrapper

--- src/components/atoms/PageWrapper.ts
+++ src/components/atoms/PageWrapper.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 
 export interface IPageWrapper {
-  fullScreen?: boolean
+  fullScreen?: boolean;
+  justify?: boolean;
 }
 
 export const PageWrapper = styled.div<IPageWrapper>`
   display: flex;
   ${({fullScreen}) => fullScreen && `height: 95vh;`}
+  ${({justify}) => justify && `justify-content: center;`}
   ${({fullScreen}) => fullScreen && `width: 98vw;`}
 `
